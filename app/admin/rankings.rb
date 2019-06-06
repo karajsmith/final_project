f63126d69f09e38266d3512435cd0e2eacb47cf0
Raghu Betina <raghu@firstdraft.com>
2019-06-06 18:42:07 +0000

    Whitelist columns in Ranking for ActiveAdmin

--- app/admin/rankings.rb
+++ app/admin/rankings.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Ranking do
+
+ permit_params :user_id, :destination_id, :rankings
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
