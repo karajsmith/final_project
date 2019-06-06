00a8146955473fb01be6da1cb0152d4035a10b70
Raghu Betina <raghu@firstdraft.com>
2019-06-06 18:41:27 +0000

    Direct association between users and friend_requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :friend_requests_rec,
+             :class_name => "FriendRequest",
+             :foreign_key => "receipient_id",
+             :dependent => :destroy
+
   has_many   :friend_requests,
              :foreign_key => "sender_id",
              :dependent => :destroy
--- spec/models/friend_request_spec.rb
+++ spec/models/friend_request_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe FriendRequest, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:receipient) }
+
     it { should belong_to(:sender) }
 
     end
