93acd87c0e4378e8b41649f409841f1991e06249
Raghu Betina <raghu@firstdraft.com>
2019-06-06 18:41:15 +0000

    Generate destinations resource with draft:resource

---
+++ app/controllers/destinations_controller.rb
@@ -0,0 +1,63 @@
+class DestinationsController < ApplicationController
+  def index
+    @destinations = Destination.all
+
+    render("destination_templates/index.html.erb")
+  end
+
+  def show
+    @destination = Destination.find(params.fetch("id_to_display"))
+
+    render("destination_templates/show.html.erb")
+  end
+
+  def new_form
+    @destination = Destination.new
+
+    render("destination_templates/new_form.html.erb")
+  end
+
+  def create_row
+    @destination = Destination.new
+
+    @destination.city = params.fetch("city")
+    @destination.country = params.fetch("country")
+
+    if @destination.valid?
+      @destination.save
+
+      redirect_back(:fallback_location => "/destinations", :notice => "Destination created successfully.")
+    else
+      render("destination_templates/new_form_with_errors.html.erb")
+    end
+  end
+
+  def edit_form
+    @destination = Destination.find(params.fetch("prefill_with_id"))
+
+    render("destination_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    @destination = Destination.find(params.fetch("id_to_modify"))
+
+    @destination.city = params.fetch("city")
+    @destination.country = params.fetch("country")
+
+    if @destination.valid?
+      @destination.save
+
+      redirect_to("/destinations/#{@destination.id}", :notice => "Destination updated successfully.")
+    else
+      render("destination_templates/edit_form_with_errors.html.erb")
+    end
+  end
+
+  def destroy_row
+    @destination = Destination.find(params.fetch("id_to_remove"))
+
+    @destination.destroy
+
+    redirect_to("/destinations", :notice => "Destination deleted successfully.")
+  end
+end
